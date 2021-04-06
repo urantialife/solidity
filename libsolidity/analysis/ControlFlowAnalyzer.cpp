3a8b84813b8c415b2cd399aeae1ae1a205fc4821
Mathias Baumann <marenz@supradigital.org>
2021-04-06 16:31:58 +0200

    ControlFlowAnalyzer: Use temporary reference for more readable code

--- libsolidity/analysis/ControlFlowAnalyzer.cpp
+++ libsolidity/analysis/ControlFlowAnalyzer.cpp
@@ -178,18 +178,20 @@ void ControlFlowAnalyzer::checkUninitializedAccess(CFGNode const* _entry, CFGNod
 
 		for (auto const* variableOccurrence: uninitializedAccessesOrdered)
 		{
+			VariableDeclaration const& varDecl = variableOccurrence->declaration();
+
 			SecondarySourceLocation ssl;
 			if (variableOccurrence->occurrence())
-				ssl.append("The variable was declared here.", variableOccurrence->declaration().location());
+				ssl.append("The variable was declared here.", varDecl.location());
 
-			bool isStorage = variableOccurrence->declaration().type()->dataStoredIn(DataLocation::Storage);
-			bool isCalldata = variableOccurrence->declaration().type()->dataStoredIn(DataLocation::CallData);
+			bool isStorage = varDecl.type()->dataStoredIn(DataLocation::Storage);
+			bool isCalldata = varDecl.type()->dataStoredIn(DataLocation::CallData);
 			if (isStorage || isCalldata)
 				m_errorReporter.typeError(
 					3464_error,
 					variableOccurrence->occurrence() ?
 						*variableOccurrence->occurrence() :
-						variableOccurrence->declaration().location(),
+						varDecl.location(),
 					ssl,
 					"This variable is of " +
 					string(isStorage ? "storage" : "calldata") +
@@ -197,14 +199,14 @@ void ControlFlowAnalyzer::checkUninitializedAccess(CFGNode const* _entry, CFGNod
 					(variableOccurrence->kind() == VariableOccurrence::Kind::Return ? "returned" : "accessed") +
 					" without prior assignment, which would lead to undefined behaviour."
 				);
-			else if (!_emptyBody && variableOccurrence->declaration().name().empty())
+			else if (!_emptyBody && varDecl.name().empty())
 			{
 				if (!m_previousVariableWarnings.emplace(&varDecl).second)
 					continue;
 
 				m_errorReporter.warning(
 					6321_error,
-					variableOccurrence->declaration().location(),
+					varDecl.location(),
 					(
 						!_contract || _contract == _function.annotation().contract ?
 						"U" :
