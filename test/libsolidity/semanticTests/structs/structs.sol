da1cf9267dc8938f6d592c06ff716599237bfc50
chriseth <chris@ethereum.org>
2021-04-14 13:58:16 +0200

    Updated gas costs.

--- test/libsolidity/semanticTests/structs/structs.sol
+++ test/libsolidity/semanticTests/structs/structs.sol
@@ -32,7 +32,7 @@ contract test {
 // ----
 // check() -> false
 // set() ->
-// gas irOptimized: 128462
+// gas irOptimized: 127575
 // gas legacy: 129577
 // gas legacyOptimized: 126964
 // check() -> true
